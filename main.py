d4e13d9a57d379630f9bb634a111fb6fd61bf682
Dasha <tikimi808@gmail.com>
2026-04-16 19:10:04 +0300

    выводить автора проекта через main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Hello from repository!")
+
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    # Загружаем переменные из файла .env
+    load_dotenv()
+    # Читаем значение переменной AUTHOR
+    author = os.getenv('AUTHOR')
+    # Печатаем имя автора
+    print(f"Автор проекта: {author}")
+
+# Вызов функции для проверки
+if __name__ == "__main__":
+    print_author()
